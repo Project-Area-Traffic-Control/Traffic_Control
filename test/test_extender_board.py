4cca555ef7bc7b67593f62e5c0217e81490b616d
Kajorn <kajornboat@gmail.com>
2022-04-21 05:27:11 +0700

    update ui

--- test/test_extender_board.py
+++ test/test_extender_board.py
@@ -22,38 +22,30 @@ class PORT_SIGNAL:
 
 port = []
 
-for i in range(15):
-
+for i in range(4):
     p = PORT_SIGNAL(mcp1.get_pin(i*3), mcp1.get_pin((i*3)+1),
                     mcp1.get_pin((i*3)+2))
     port.append(p)
 
 
-PA1_R = mcp1.get_pin(0)
-PA1_G = mcp1.get_pin(1)
-PA1_Y = mcp1.get_pin(2)
+p5 = PORT_SIGNAL(mcp1.get_pin(15), mcp2.get_pin(0),
+                    mcp2.get_pin(1))
 
-PA2_R = mcp1.get_pin(3)
-PA2_G = mcp1.get_pin(4)
-PA2_Y = mcp1.get_pin(5)
+port.append(p5)
 
-PA3_R = mcp1.get_pin(6)
-PA3_G = mcp1.get_pin(7)
-PA3_Y = mcp1.get_pin(8)
 
-PA4_R = mcp1.get_pin(9)
-PA4_G = mcp1.get_pin(10)
-PA4_Y = mcp1.get_pin(11)
-
-PA5_R = mcp1.get_pin(12)
-PA5_G = mcp1.get_pin(13)
-PA5_Y = mcp1.get_pin(14)
-
-pin0.switch_to_output(value=True)
-
+port[0].switch_to_output(value=True)
 
 while True:
-    pin0.value = True
+    for p in port:
+        p.red.value = True
+        p.green.value = True
+        p.yellow.value = True
+    
     time.sleep(0.5)
-    pin0.value = False
+    for p in port:
+        p.red.value = False
+        p.green.value = False
+        p.yellow.value = False
+    
     time.sleep(0.5)
